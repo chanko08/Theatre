8d68620e052756a662379a97342dcf10260aa96f
Alex Bechanko <chanko08@gmail.com>
2012-12-11 00:43:48 -0500

    Updated comments on entity class

--- theatre/entity.py
+++ theatre/entity.py
@@ -46,15 +46,33 @@ class Entity(object):
         self._groups.extend(groups)
 
     def __getitem__(self, key):
+        """
+            Allows easy access to components via
+                entity['all lower case component class name']
+        """
         return self._components[key]
 
     def __getattr__(self, name):
+        """
+            If there is no attribute attached to the entity with this
+            name, then check if any component has this name, and access it.
+            This specifically will work for component class names in all lowercase.
+        """
         if name in self._components:
             return self._components[name]
         raise AttributeError("Couldn't find component with name:{0}".format(name))
 
     def member_of(self, group):
+        """
+            A test of whether the entity holds a given group identifier
+        """
         return group in self._groups
 
     def delete(self):
+        """
+            Marks the entity for deletion for the scene.
+            If an entity needs to clean anything up prior to deletion
+            it is recommended to overload this method, and in the overloaded
+            method to call the Entity version of the method via super
+        """
         self._dead = True
